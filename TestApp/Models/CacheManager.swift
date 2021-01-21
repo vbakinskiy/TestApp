6b5815e017f5a63419834ce61b5b3d78edbcc5c1
Vyacheslav Bakinskiy <vyacheslav.bakinskiy@gmail.com>
2021-01-21 18:48:32 +0300

    Add CacheManager

---
+++ TestApp/Models/CacheManager.swift
@@ -0,0 +1,31 @@
+//
+//  FileManager.swift
+//  TestApp
+//
+//  Created by Vyacheslav Bakinskiy on 1/21/21.
+//
+
+import UIKit
+
+class CacheManager {
+    private static let directoryURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+    
+    static func getCachedImage(withName: String) -> UIImage? {
+        let fileURL = directoryURL.appendingPathComponent(withName)
+        do {
+            let data = try Data(contentsOf: fileURL)
+            return UIImage(data: data)
+        } catch {
+            return nil
+        }
+    }
+    
+    static func saveImageToCache(data: Data, withName: String) {
+        let fileURL = directoryURL.appendingPathComponent(withName)
+        do {
+            try data.write(to: fileURL)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
