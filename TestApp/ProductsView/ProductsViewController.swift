6a499523d03d4ab3366a50868f2cf887cbf10833
Vyacheslav Bakinskiy <vyacheslav.bakinskiy@gmail.com>
2021-01-21 11:32:40 +0300

    Create ProductCell

---
+++ TestApp/ProductsView/ProductsViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ProductsViewController.swift
+//  TestApp
+//
+//  Created by Vyacheslav Bakinskiy on 1/21/21.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+    
+    //MARK: - Private properties
+    
+    private var numberOfItemsPerRow: CGFloat {
+        switch UIDevice.current.orientation{
+        case .portrait, .portraitUpsideDown:
+            return 2
+        case .landscapeLeft, .landscapeRight:
+            return 4
+        default:
+            return 2
+        }
+    }
+    
+    private let spacing: CGFloat = 16.0
+    
+    //MARK: - @IBOutlets
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerCell()
+        setupCollectionViewLoyout()
+    }
+    
+    //MARK: - Private funcs
+    
+    private func registerCell() {
+        collectionView.register(UINib(nibName: "ProductCell", bundle: nil), forCellWithReuseIdentifier: ProductCell.reuseId)
+    }
+    
+    private func setupCollectionViewLoyout() {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        self.collectionView?.collectionViewLayout = layout
+    }
+    
+    //MARK: - Interface orientation
+    
+    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        collectionView.reloadData()
+    }
+}
+
+//MARK: - UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+
+extension ProductsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductCell.reuseId, for: indexPath) as! ProductCell
+        cell.productName.text = "Some product name"
+        cell.productPrice.text = "10000"
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let totalSpacing = (2 * spacing) + ((numberOfItemsPerRow - 1) * spacing)
+        let width = (collectionView.bounds.width - totalSpacing) / numberOfItemsPerRow
+        
+        return CGSize(width: width, height: width)
+    }
+}
+
