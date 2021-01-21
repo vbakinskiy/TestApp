03a86a8d59706a1908bb6a2b4778af9f0cbc30f6
Vyacheslav Bakinskiy <vyacheslav.bakinskiy@gmail.com>
2021-01-21 18:46:41 +0300

    Add ProductViewModel

---
+++ TestApp/ViewModels/ProductViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ProductViewModel.swift
+//  TestApp
+//
+//  Created by Vyacheslav Bakinskiy on 1/21/21.
+//
+
+import UIKit
+
+struct ProductViewModel {
+    var productImage: String?
+    var productName: String?
+    var productPrice: Int?
+}
