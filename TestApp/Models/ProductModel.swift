e166a75feaa49a10c5835a7f31233687a689cb0c
Vyacheslav Bakinskiy <vyacheslav.bakinskiy@gmail.com>
2021-01-21 18:45:42 +0300

    Add ProductModel

---
+++ TestApp/Models/ProductModel.swift
@@ -0,0 +1,19 @@
+//
+//  JsonModel.swift
+//  TestApp
+//
+//  Created by Vyacheslav Bakinskiy on 1/21/21.
+//
+
+import Foundation
+
+struct Products: Codable {
+    let products: [Product]
+}
+
+struct Product: Codable {
+    let productId: String?
+    let name: String?
+    let price: Int?
+    let image: String?
+}
